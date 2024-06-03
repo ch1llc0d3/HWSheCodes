bdbb5f1edb09618a8738ce9ee95ae5b2463ec463
ch1llc0d3 <128004915+ch1llc0d3@users.noreply.github.com>
2024-06-02 21:14:01 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "4b8t3575997a0fbfef8539da07c4o512";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
